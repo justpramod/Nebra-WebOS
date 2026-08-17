861ac2673a27d27c87f80492c8fee6f7ada2059e
Kapil Gaire <eriaglipak312@gmail.com>
2026-08-17 18:10:04 +0545

    add: subtract opcode handler

--- backend/os_engine/opcode_handlers.py
+++ backend/os_engine/opcode_handlers.py
@@ -13,9 +13,23 @@ def handle_add(cpu, process, operands: list):
     cpu.program_counter += 1
 
 
+def handle_subtract(cpu, process, operands: list):
+    r0, r1, r2 = operands
+
+    cpu.registers[reg_index(r0)] = (
+        cpu.registers[reg_index(r1)] - cpu.registers[reg_index(r2)]
+    )
+
+    cpu.program_counter += 1
+
+
 def handle_halt(cpu, process, operands: list):
     # syscall to kernel, which reclaims its resources. implement later.
     process.transition_to(ProcessState.TERMINATED)
 
 
-OPCODE_HANDLERS = {Opcode.ADD: handle_add, Opcode.HALT: handle_halt}
+OPCODE_HANDLERS = {
+    Opcode.ADD: handle_add,
+    Opcode.HALT: handle_halt,
+    Opcode.SUBTRACT: handle_subtract,
+}
