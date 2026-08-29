11ad883be9ccecce1507faafecba71da7276dc8e
Kapil Gaire <eriaglipak312@gmail.com>
2026-08-29 11:38:48 +0545

    add: added three construct methods to initialize pte and a method to get the bytes

--- backend/os_engine/memory/pte.py
+++ backend/os_engine/memory/pte.py
@@ -18,19 +18,72 @@ total bytes: 3 bytes -> 24 bits
 
 """
 
+from dataclasses import dataclass
 
+
+@dataclass()
 class PTE:
-    def __init__(self, byte_1: int, byte_2: int, byte_3: int) -> None:
-        pte_data = (
-            (byte_1 << 16) | (byte_2 << 8) | byte_3
-        )  # left shift and combine with bitwise OR
+    pte_data_block: int | None = None
+    physical_frame_number: int | None = None
+    present_flag: bool | None = None
+    read_write_flag: bool | None = None
+    user_kernel_flag: bool | None = None
 
+    def _initialize_entries_from_block(self, pte_data_block):
+        """
+        protected method to initialize entries of block.
+        """
         flag_bits = (
-            pte_data & 0xF
+            pte_data_block & 0xF
         )  # masking with 1111, so only the last 4 bits are preserved
 
-        self.physical_frame_number = pte_data >> 4
+        self.pte_data_block = pte_data_block
+        self.physical_frame_number = pte_data_block >> 4
 
         self.present_flag = bool(flag_bits >> 3)
         self.read_write_flag = bool((flag_bits >> 2) & 1)
         self.user_kernel_flag = bool((flag_bits >> 1) & 1)
+
+    def construct_from_bytes(self, byte_1: int, byte_2: int, byte_3: int) -> None:
+        pte_data_block = (
+            (byte_1 << 16) | (byte_2 << 8) | byte_3
+        )  # left shift and combine with bitwise OR
+
+        self._initialize_entries_from_block(pte_data_block)
+
+    def construct_from_pte_data_block(self, pte_data_block):
+        self._initialize_entries_from_block(pte_data_block)
+
+    def construct_from_data_entries(
+        self,
+        physical_frame_number: int,
+        present_flag: bool,
+        read_write_flag: bool,
+        user_kernel_flag: bool,
+    ):
+        present_flag_bit: int = 1 if present_flag else 0
+
+        read_write_flag_bit: int = 1 if read_write_flag else 0
+
+        user_kernel_flag_bit: int = 1 if user_kernel_flag else 0
+
+        pte_data_block = physical_frame_number << 4
+        pte_data_block |= present_flag_bit << 3
+        pte_data_block |= read_write_flag_bit << 2
+        pte_data_block |= user_kernel_flag_bit << 1
+
+        self._initialize_entries_from_block(pte_data_block)
+
+
+
+    def get_pte_bytes(self)-> list[int]:
+        if self.pte_data_block:
+            byte_1 :int = (self.pte_data_block >> 16) & 0xFF
+            byte_2 :int = ( self.pte_data_block >> 8) >> 0xFF
+            byte_3 :int = self.pte_data_block & 0xFF
+
+            return [byte_1, byte_2, byte_3]
+
+        else :
+            raise Exception("PTE block values are not initialized.")
+
